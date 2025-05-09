9ca80883fc08a098c71bfcf1b70bb58bd3a3f824
Devang Narayan <devangnpillai@gmail.com>
2025-05-09 12:18:15 +0530

    added Day 1 files

---
+++ day-1-code.py
@@ -0,0 +1,39 @@
+# Day 1 Code
+# Task 1: Printing
+
+print("Hello world!")
+
+# Task 2: String Manipulation
+
+print("Hello world!")
+print("Hello" + " " + "Devang")
+
+# Task 3: Inputs
+
+print("Hello" + " " + input("What is your name? ") + "!")
+
+# Task 4: Variables
+
+name = input("What is your name?")
+print(name)
+num_charecs = len(name)
+print(num_charecs)
+
+# Task 5: Variable Naming
+
+name = "Devang"
+length = len(name)
+print(length)
+
+# Task 6: Project - Band Name Generater
+
+print("Hello: Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+print('Your band name is:' + " " + city + " " + pet)
+
+# DAY 1 END
+# ----------------------------------------------
+
+
+
