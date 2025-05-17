67fb13c0a34b8265f34ad49e54d6abfe087e03fa
Devang Narayan <devangnpillai@gmail.com>
2025-05-17 12:38:47 +0530

    added day-1 project to the repo

---
+++ day-1/day-1-proj.py
@@ -0,0 +1,11 @@
+# Project - Band Name Generater
+
+print("Hello: Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+print('Your band name is:' + " " + city + " " + pet)
+
+# ----------------------------------------------
+
+
+
